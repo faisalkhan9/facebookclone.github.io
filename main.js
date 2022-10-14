bf184b97f35365f941928cdbb86594433ac6a0cf
faisalkhan9 <86875342+faisalkhan9@users.noreply.github.com>
2022-10-14 11:22:43 +0530

    index.html

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}  
+
+/*
+
+$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+*/
